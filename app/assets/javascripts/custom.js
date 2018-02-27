c25bf5c4b7cb1724f1cd9f698f43673f9a1eee20
Justine <>
2018-02-27 11:17:56 +0100

    js finally fixed

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -27,8 +27,8 @@ var arrayNumbers = [];
 var numbers = ['0','1','2','3','4','5','6','7','8','9'];
 var actions = ['+','-','x','/'];
 
-function test() {
-	x = $(this).text();
+function test(x) {
+	console.log(x);
 
 // Si le choix est un nombre
 	if (numbers.includes(x)) {
@@ -44,14 +44,13 @@ function test() {
 		else {
 			arrayNumbers[arrayNumbers.length-1] += x;
 		}
-		$('#calcul').text(arrayNumbers[arrayNumbers.length-1]);
+		document.getElementById("calcul").innerHTML = arrayNumbers[arrayNumbers.length-1];
 	}
 
 // Si on clique sur le point
 	else if (x === ".") {
 		if (arrayNumbers[arrayNumbers.length-1].includes('.')){
-			$('#calcul').text(arrayNumbers[arrayNumbers.length-1]);
-
+		document.getElementById("calcul").innerHTML = arrayNumbers[arrayNumbers.length-1];
 		}
 		else {
 // Si le dernier clic était une action, on met un zéro devant le point dans une nouvelle case		
@@ -60,42 +59,39 @@ function test() {
 		}
 	}
 
-
 	else if (actions.includes(x)) {
 		if (actions.includes(arrayNumbers[arrayNumbers.length-1])) {
 			arrayNumbers.splice(arrayNumbers.length - 1,1);
-			arrayNumbers.push(x = $(this).text());
-			$('#calcul').text(arrayNumbers[arrayNumbers.length-1]);
+			arrayNumbers.push(x);
+			document.getElementById("calcul").innerHTML = arrayNumbers[arrayNumbers.length-1];
 
 		}
 		else {
-			arrayNumbers.push(x = $(this).text());
-			$('#calcul').text(arrayNumbers[arrayNumbers.length-1]);
+			arrayNumbers.push(x);
+			document.getElementById("calcul").innerHTML = arrayNumbers[arrayNumbers.length-1];
 		}
 	}
 
 
 	else if (x === "=") {
 		if (arrayNumbers[1] === "+") {
-			$('#calcul').text(add(Number(arrayNumbers[0]),Number(arrayNumbers[2])));
+			document.getElementById("calcul").innerHTML = add(Number(arrayNumbers[0]),Number(arrayNumbers[2]));
 		}
 		else if (arrayNumbers[1] === "-") {
-			$('#calcul').text(substract(Number(arrayNumbers[0]),Number(arrayNumbers[2])));
+			document.getElementById("calcul").innerHTML = add(Number(arrayNumbers[0]),Number(arrayNumbers[2]));
 		}
 		else if (arrayNumbers[1] === "x") {
-			$('#calcul').text(multiply(Number(arrayNumbers[0]),Number(arrayNumbers[2])));
+			document.getElementById("calcul").innerHTML = add(Number(arrayNumbers[0]),Number(arrayNumbers[2]));
 		}
 		else if (arrayNumbers[1] === "/") {
-			$('#calcul').text(divide(Number(arrayNumbers[0]),Number(arrayNumbers[2])));
+			document.getElementById("calcul").innerHTML = add(Number(arrayNumbers[0]),Number(arrayNumbers[2]));
 		}
 		arrayNumbers = []
 	}
 
 	else if (x === "clear") {
 		arrayNumbers = [];
-		$('#calcul').text('0');
+		document.getElementById("calcul").innerHTML = '0';
 	}
 }
-
-
 
